be6dacd3482c5dc0ed2792f19c7b5196bd8115be
Salim Benfarhat <hello@salim.link>
2024-11-27 08:47:23 +0100

    fix(redux): dispatch loadAuthState in store

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -3,6 +3,7 @@ import { configureStore } from '@reduxjs/toolkit';
 import authSlice from './slices/authSlice';
 import userProfileSlice from './slices/userProfileSlice';
 import { middleware } from './middleware'; 
+import { loadAuthState } from './thunks/loadAuthStateThunk';
 
 // Configure le store Redux avec Redux Toolkit
 // configureStore est une fonction de Redux Toolkit qui permet de configurer le store Redux 
@@ -14,6 +15,11 @@ const store = configureStore({
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(middleware) 
 });
+
+// Charger l'état d'authentification au démarrage de l'application
+// On dispatch loadAuthState au démarrage pour vérifier si un token est présent 
+// dans le localStorage ou le sessionStorage et pour mettre à jour l'état d'authentification en conséquence.
+store.dispatch(loadAuthState());
 
 // Export du store
 export default store;
