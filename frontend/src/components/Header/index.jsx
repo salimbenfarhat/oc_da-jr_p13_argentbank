b3a662a9b25b7d31fc41752276d5263a643ae933
Salim Benfarhat <hello@salim.link>
2024-11-27 09:09:48 +0100

    feat(components): implement logout functionality in Header

--- frontend/src/components/Header/index.jsx
+++ frontend/src/components/Header/index.jsx
@@ -1,9 +1,26 @@
+// src/components/Header/index.jsx
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link, NavLink } from 'react-router-dom';
 import ARGENTBANK_LOGO from '../../assets/img/argentBankLogo.png';
+import { logout } from '../../redux/slices/authSlice'
 import '../Header/index.scss';
 
 function Header() {
+  // Hook pour dispatcher les actions Redux
+  const dispatch = useDispatch();
+  // Hook pour accéder à l'état d'authentification depuis le store Redux
+  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+  // Hook pour accéder au prénom de l'utilisateur connecté depuis le store Redux
+  const userFirstName = useSelector((state) => state.userProfile.firstName);
+
+  // Fonction gérant le clic sur le bouton de déconnexion.
+  // Empêche le comportement par défaut du lien et dispatch l'action de déconnexion.
+  const handleLogout = (e) => {
+    e.preventDefault();
+    dispatch(logout());
+  };
+
   return (
     <nav className="main-nav">
       <Link className="main-nav-logo" to="/">
@@ -15,9 +32,24 @@ function Header() {
         <h1 className="sr-only">Argent Bank</h1>
       </Link>
       <div>
-        <NavLink className="main-nav-item" to="/login">
-          <i className="fa fa-user-circle"></i> Sign In
-        </NavLink>
+        {/* Affichage conditionnel basé sur l'état d'authentification */}
+        {isAuthenticated ? (
+          // Si l'utilisateur est connecté, afficher son prénom et le bouton de déconnexion
+          <>
+            <i className="fa fa-user-circle main-nav-item"></i>
+            <span className="main-nav-item">{userFirstName}</span>
+            <NavLink className="main-nav-item" to="/login" onClick={handleLogout}>
+              <i className="fa fa-sign-out"></i> Sign Out
+            </NavLink>
+          </>
+        ) : (
+          // Sinon, afficher le lien de connexion
+          <>
+            <NavLink className="main-nav-item" to="/login">
+              <i className="fa fa-user-circle"></i> Sign In
+            </NavLink>
+          </>
+        )}
       </div>
     </nav>
   );
